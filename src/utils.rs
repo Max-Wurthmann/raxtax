a45406e2709a293c619ed80b7b16286e6561bb25
Max-Wurthmann <mx.wurthmann@gmail.com>
2026-06-04 18:23:08 +0200

    added scaffolding for encode function

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::HashSet,
     fs::File,
+    hint::unreachable_unchecked,
     io::{BufReader, Read},
     path::PathBuf,
 };
@@ -32,7 +33,7 @@ pub struct BitMasks {
 
 impl BitMasks {
     pub fn new(k: u32) -> Option<Self> {
-        if k < 1 || k > 16 || k % 2 == 0 {
+        if (1..=16).step_by(2).contains(&k) {
             return None;
         }
 
@@ -43,23 +44,46 @@ impl BitMasks {
     }
 }
 
+fn encode(kmer: u32, rev_compl_kmer: u32, bitmasks: &BitMasks) -> u32 {
+    let k = bitmasks.k;
+    let common_prefix_length = (kmer ^ rev_compl_kmer).leading_zeros();
+    let mut code = 0u32;
+
+    if common_prefix_length < k as u32 - 1 {
+        // specifieing pair
+
+        // TODO:
+        unimplemented!()
+    } else if common_prefix_length == k as u32 - 1 {
+        // only differs in middle char
+
+        // TODO:
+        unimplemented!()
+    } else {
+        debug_assert!(false, "palindrome can not occur with odd k");
+        unsafe { unreachable_unchecked() };
+    }
+
+    code
+}
+
 /// Extracts canonical k-mers from the given sequence.
 /// where k is odd and 1 <= k <= 16.
 /// Invalid characters are ignored, any k-mer containing an invalid character is skipped.
 /// Canonical k-mers are encoded using a minimal encoding scheme and are thus in the range [0, 1/2 * 4^k).
 /// (see Wittler R. General encoding of canonical k-mers. Peer Community Journal. 2023)
 /// May yield duplicate k-mers, use `seq_to_unique_canon_kmers` to get unique sorted k-mers.
-pub fn seq_to_minenc_canon_kmer_iter(
-    sequence: &[u8],
-    bitmasks: BitMasks,
-) -> impl Iterator<Item = u32> + use<'_> {
+pub fn seq_to_minenc_canon_kmer_iter<'a, 'b>(
+    sequence: &'a [u8],
+    bitmasks: &'b BitMasks,
+) -> impl Iterator<Item = u32> + use<'a, 'b> {
     let mut seq_iter = sequence.iter();
     let mut kmer = 0_u32;
     let mut rev_compl_kmer = 0_u32;
     let mut filled_bases = 0_u8; // Tracks how many consecutive valid bases we have processed
 
     std::iter::from_fn(move || {
-        while let Some(char) = seq_iter.next() {
+        for char in seq_iter.by_ref() {
             if let Some(repr) = map_four_to_two_bit_repr(*char) {
                 // Add repr to the end of kmer
                 kmer = (kmer << 2) | repr as u32;
@@ -71,12 +95,10 @@ pub fn seq_to_minenc_canon_kmer_iter(
                 // Add complement_repr to the start of rev_compl_kmer
                 rev_compl_kmer = (rev_compl_kmer >> 2) | (complement_repr << (bitmasks.k * 2 - 2));
 
-                let common_prefix_length = (kmer ^ rev_compl_kmer).leading_zeros();
-
                 filled_bases += 1;
                 if filled_bases >= bitmasks.k {
                     // Only yield a valid k-mer once our sliding window has 8 consecutive valid bases
-                    return Some(std::cmp::min(kmer, rev_compl_kmer));
+                    return Some(encode(kmer, rev_compl_kmer, bitmasks));
                 }
             } else {
                 // Invalid/ambiguous char encountered
